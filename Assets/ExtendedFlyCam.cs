8849b6a0c9ca9e7a52ec9518498d66e482509aa1
Andrew Wang <andyman@idumpling.com>
2017-01-24 17:34:18 -1000

    Some fixes for release 1.0.1

---
+++ Assets/ExtendedFlyCam.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtendedFlyCam : MonoBehaviour
+{
+
+	public float cameraSensitivity = 90;
+	public float climbSpeed = 4;
+	public float normalMoveSpeed = 10;
+	public float slowMoveFactor = 0.25f;
+	public float fastMoveFactor = 3;
+	
+	public float rotationX = 0.0f;
+	public float rotationY = 0.0f;
+	
+	void Start ()
+	{
+	}
+
+	void OnEnable()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+	void OnDisable()
+	{
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+	}
+	void Update ()
+	{
+		Vector3 forwardVector = transform.rotation * Vector3.forward;
+		transform.rotation = Quaternion.LookRotation (forwardVector, Vector3.up);
+
+		transform.rotation *= Quaternion.AngleAxis((Input.GetAxis ("Mouse X")) * cameraSensitivity * Time.unscaledDeltaTime, Vector3.up);
+		transform.rotation *= Quaternion.AngleAxis((Input.GetAxis ("Mouse Y")) * cameraSensitivity * Time.unscaledDeltaTime, Vector3.left);
+
+		float forwardBack = 0.0f;
+		float leftRight = 0.0f;
+		if (Input.GetKey (KeyCode.W)) forwardBack += 1.0f;
+		if (Input.GetKey (KeyCode.S)) forwardBack -= 1.0f;
+		if (Input.GetKey (KeyCode.A)) leftRight -= 1.0f;
+		if (Input.GetKey (KeyCode.D)) leftRight += 1.0f;
+
+		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift))
+		{
+			transform.position += transform.forward * (normalMoveSpeed * fastMoveFactor) * forwardBack * Time.unscaledDeltaTime;
+			transform.position += transform.right * (normalMoveSpeed * fastMoveFactor) * leftRight * Time.unscaledDeltaTime;
+		}
+		else if (Input.GetKey (KeyCode.LeftControl) || Input.GetKey (KeyCode.RightControl))
+		{
+			transform.position += transform.forward * (normalMoveSpeed * slowMoveFactor) * forwardBack * Time.unscaledDeltaTime;
+			transform.position += transform.right * (normalMoveSpeed * slowMoveFactor) * leftRight * Time.unscaledDeltaTime;
+		}
+		else
+		{
+			transform.position += transform.forward * normalMoveSpeed * forwardBack * Time.unscaledDeltaTime;
+			transform.position += transform.right * normalMoveSpeed * leftRight * Time.unscaledDeltaTime;
+		}
+		
+		if (Input.GetKey (KeyCode.E)) {transform.position += transform.up * climbSpeed * Time.unscaledDeltaTime;}
+		if (Input.GetKey (KeyCode.Q)) {transform.position -= transform.up * climbSpeed * Time.unscaledDeltaTime;}
+
+
+		if (Input.GetKeyDown(KeyCode.Equals))
+		{
+			Camera.main.fieldOfView += 5.0f;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Minus))
+		{
+			Camera.main.fieldOfView -= 5.0f;
+		}
+
+		if (Input.GetKeyDown (KeyCode.F))
+		{
+			RenderSettings.fog = !RenderSettings.fog;
+		}
+	}
+}
---
+++ Assets/Scripts/ScreenshotPauser.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScreenshotPauser : MonoBehaviour {
+
+	public int superSize = 10;
+
+	public bool paused = false;
+
+	public KeyCode pauseKey = KeyCode.P;
+	public KeyCode toggleEnvironmentKey = KeyCode.O;
+
+	public GameObject[] environment;
+	public GameObject screenshotPanel;
+
+	public float normalTimeScale = 1.0f;
+	[SerializeField]
+	public UnityEvent onPause;
+
+	[SerializeField]
+	public UnityEvent onResume;
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = normalTimeScale;
+	}
+
+	public void Restart() {
+		SceneManager.LoadScene(0);
+	}
+	public void Toggle()
+	{
+		if (paused)
+		{
+			paused = false;
+			Time.timeScale = normalTimeScale;
+			onResume.Invoke();
+		}
+		else
+		{
+			paused = true;
+			Time.timeScale = 0.0f;
+			onPause.Invoke ();
+		}
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(pauseKey))
+		{
+			Toggle();
+		}
+		if (takeHiResShot)
+		{
+			ticks++;
+			if (screenshotTaken && ticks > 5)
+			{
+				takeHiResShot = false;
+				screenshotTaken = false;
+				ticks = 0;
+
+				if (screenshotPanel != null)
+				{
+					screenshotPanel.SetActive(true);
+				}
+			}
+		}
+
+
+		if (paused && !takeHiResShot && Input.GetKeyDown(KeyCode.Return))
+		{
+			#if UNITY_STANDALONE
+				takeHiResShot = true;
+				screenshotTaken = false;
+				if (screenshotPanel != null)
+				{
+					screenshotPanel.SetActive(false);
+				}
+				ticks = 0;
+			#else
+				if (screenshotLocationText != null)
+				{
+					screenshotLocationText.text = "Sorry, the screenshot key only works for standalone builds.\nYou'll need to manually take a screenshot.";
+				}
+			#endif
+		}
+
+
+		if (Input.GetKeyDown (toggleEnvironmentKey))
+		{
+			for(int i = 0; i < environment.Length; i++)
+			{
+				environment[i].SetActive (!environment[i].activeSelf);
+			}
+		}
+	}
+	int ticks = 0;
+
+	bool takeHiResShot = false;
+	bool screenshotTaken = false;
+
+	public static string ScreenShotName() {
+
+		return string.Format("{0}/screenshot_{1}.png", 
+			System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop),
+		                     System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+	}
+	
+	public void TakeHiResShot() {
+		takeHiResShot = true;
+	}
+
+	public AudioClip cameraSound;
+
+	void LateUpdate() {
+		if (takeHiResShot && ticks > 5 && !screenshotTaken) {
+			string filename = ScreenShotName();
+			Application.CaptureScreenshot (filename, superSize);
+            if (cameraSound != null)
+            {
+                AudioSource.PlayClipAtPoint(cameraSound, Camera.main.transform.position, 1.0f);
+            }
+			Debug.Log(string.Format("Took screenshot to: {0}", filename));
+
+			screenshotTaken = true;
+			ticks = 0;
+
+			if (screenshotLocationText != null)
+			{
+				screenshotLocationText.text = "Captured: " + filename;
+			}
+		}
+	}
+
+	public Text screenshotLocationText;
+
+}
